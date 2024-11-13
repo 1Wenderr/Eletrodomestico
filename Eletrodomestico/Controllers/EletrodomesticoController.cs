3d101d35ea20485628079ec9b7b617a3fe04a20f
1Wenderr <wender.penha@aluno.senai.br>
2024-11-13 07:35:06 -0300

    Adicionar arquivos de projeto.

---
+++ Eletrodomestico/Controllers/EletrodomesticoController.cs
@@ -0,0 +1,22 @@
+﻿using Eletrodomestico.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eletrodomestico.Controllers
+{
+    public class EletrodomesticoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Eletrodomestico.Models.Eletrodomestico> eletrodomesticos = new List<Eletrodomestico.Models.Eletrodomestico>
+            {
+                new Geladeira {Marca = "Samsung", Modelo = "Side by Side", ConsumoEnergetico = 40, CapacidadeLitros = 500, TemFreezer = true },
+                new Geladeira {Marca = "Brastemp", Modelo = "Frost Free", ConsumoEnergetico = 30, CapacidadeLitros = 435, TemFreezer = true },
+                new Geladeira {Marca = "Consul", Modelo = "Frost Free", ConsumoEnergetico = 40, CapacidadeLitros = 443, TemFreezer = true },
+                new Televisor {Marca = "Samsung", Modelo = "QLED Q60A", ConsumoEnergetico = 85, TamanhoTela = 50, EhSmart = true},
+                new Televisor {Marca = "TCL", Modelo = "4K Smart TV", ConsumoEnergetico = 70, TamanhoTela = 43, EhSmart = true},
+                new Televisor {Marca = "LG", Modelo = "OLED CX 4K Gaming", ConsumoEnergetico = 150, TamanhoTela = 55, EhSmart = false},
+            };
+            return View(eletrodomesticos);
+        }
+    }
+}
---
+++ Eletrodomestico/Models/Eletrodomestico.cs
@@ -0,0 +1,11 @@
+﻿namespace Eletrodomestico.Models
+{
+    public abstract class Eletrodomestico
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public double ConsumoEnergetico { get; set; }
+
+        public abstract string ExibirDetalhes();
+    }
+}
